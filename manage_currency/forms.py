f495539c65c35fc4d147c726f027162aec398042
nikedsb <ikedanao1230sbsnkr@gmail.com>
2022-05-15 23:50:10 +0900

    SginUpForm

--- manage_currency/forms.py
+++ manage_currency/forms.py
@@ -1,2 +1,9 @@
-from dataclasses import field
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Member
 
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = Member
+        fields = ("username", "job", "password1", "password2")
